d58223113cc656d3e65a01271e2a86727d377eda
NoSouless <leo.caselato@gmail.com>
2026-04-15 23:53:44 -0300

    traducao da pagina de estatisticas

--- source/Controllers/statisticsController.php
+++ source/Controllers/statisticsController.php
@@ -9,13 +9,15 @@ class statisticsController extends Core {
     public function __construct($router = ROOT) {
         parent::__construct($router);
         $this->validaAcesso();
-        $this->getLayout()->setHeader($this->getLogado() ? 'header-logado' : 'header');
+        $this->getLayout()->setHeader('header-logado');
         $this->getLayout()->setFooter('footer');
-        $this->addLayout();
     }
 
     public function home() {
+        $this->addTranslator('statistics/home');
+        $this->addLayout($this->getTranslator()->translate("Estatísticas"));
         $statisticsModel = new Statistics();
+        $statisticsModel->setTranslator($this->getTranslator());
         $store = $this->getUser()['loja_id'] ?? 0;
         $periodoSelecionado = $_GET['period'] ?? 'day';
         $dataReferencia = $_GET['date'] ?? date('Y-m-d');
@@ -30,12 +32,6 @@ class statisticsController extends Core {
         $customGraphs = $statisticsModel->getGraphData($store, $graphs, $periodos[$periodoSelecionado], $dataReferencia);
 
         echo $this->view->render("statistics/home", [
-            'layout' => [
-                'title' =>  'Estatísticas - Artistar', 
-                'logado' => $this->getLogado(),
-                'header' => true,
-                'footer' => true
-            ],
             'periodos' => $periodos,
             'periodoSelecionado' => $periodoSelecionado,
             'dataReferencia' => $dataReferencia,
@@ -50,12 +46,14 @@ class statisticsController extends Core {
     }
 
     public function editGraph($post) {
+        $this->addTranslator('statistics/home');
+        $tradutor = $this->getTranslator();
         $statisticsModel = new Statistics();
-        if (empty($post['graph-id']) || $post['graph-id'] < 1 || $post['graph-id'] > 6) exit($this->renderApiResponse(400, "Gráfico não encontrado."));
+        if (empty($post['graph-id']) || $post['graph-id'] < 1 || $post['graph-id'] > 6) exit($this->renderApiResponse(400, $tradutor->translate("Gráfico não encontrado.")));
         $graphId = $post['graph-id'];
 
         $store = $this->getUser()['loja_id'] ?? 0;
-        if (empty($store)) exit($this->renderApiResponse(400, "Loja não encontrada."));
+        if (empty($store)) exit($this->renderApiResponse(400, $tradutor->translate("Loja não encontrada.")));
 
         $info = [
             'type' => $post['graph-type'] ?? 'pie',
@@ -76,9 +74,9 @@ class statisticsController extends Core {
             } else {
                 $statisticsModel->updateGraphType($store, $graphId, $info);
             }
-            exit($this->renderApiResponse(200, "Gráfico atualizado com sucesso."));
+            exit($this->renderApiResponse(200, $tradutor->translate("Gráfico atualizado com sucesso.")));
         } catch (\Exception $e) {
-            exit($this->renderApiResponse(500, "Erro ao atualizar gráfico: ". $e->getMessage()));
+            exit($this->renderApiResponse(500, $tradutor->translate("Erro ao atualizar gráfico: ") . $e->getMessage()));
         }
 
         return;
